b86d6045fe88b3639ceebdebab64960b8eb9078e
CONSULTANTS1 <consultants1@jsjohnson.com>
2020-05-20 01:43:57 -0400

    new

--- src/app/quotepage/driverdialog/driverdialog.component.ts
+++ src/app/quotepage/driverdialog/driverdialog.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { DatePipe } from '@angular/common';
+import {dataformservice} from '../quotepage.component';
+import {driverservice} from '../quotepage.component';
+
 @Component({
   selector: 'app-driverdialog',
   templateUrl: './driverdialog.component.html',
@@ -13,9 +16,14 @@ export class DriverdialogComponent implements OnInit {
   datePipe = new DatePipe('en-US'); 
   wasFormChanged = false;
   Driverclient: boolean;
+  driverdata: any;
+  abc: any;
+  ngModelChecked = false;
   constructor(
     @Inject(MAT_DIALOG_DATA) public data,
     private formBuilder: FormBuilder,
+    private dataformservice:dataformservice,
+    private driverservice: driverservice,
     public dialogRef :MatDialogRef<DriverdialogComponent>,) {
 
     this.driverForm = new FormGroup({
@@ -35,7 +43,11 @@ export class DriverdialogComponent implements OnInit {
 
   ngOnInit() {
     this.breakpoint = window.innerWidth <= 790 ? 1 : 2; //
-    console.log("popup ",this.data.DriverName);
+    //console.log("popup ");
+    this.dataformservice.$datas.subscribe((data)=>{
+      var con = data;
+     // console.log("popup",con);
+    });
   }
   public onResize(event: any): void {
     this.breakpoint = event.target.innerWidth <= 590 ? 1 : 2
@@ -43,10 +55,31 @@ export class DriverdialogComponent implements OnInit {
   public get driverform(){
     return this.driverForm.controls;
   }
-  // public onSubmit() {
-  //   console.log("driver",this.driverform.DriverName.value);
-  // }
+  
+
+  onNgModelChangeEvent(e) {
+    // e is a boolean, true if checked, otherwise false
+    this.ngModelChecked = e;
+
+    if (this.ngModelChecked) {
+      var abc= this.dataformservice.dataform;
+      var name = abc.value.firstName+ " " + abc.value.lastName;
+      this.driverForm.get('DriverName').setValue([name]);
+      this.driverForm.get('DOB').setValue(abc['value']['dab']);
+      //localStorage.setItem("permission", "sd");
+    }
+    else{
+      console.log("popupgood"); 
+      this.driverForm.get('DriverName').setValue([""]);
+      this.driverForm.get('DOB').setValue([""]);
+    }
+  }
+
   dig(){
+    this.dataformservice.$datas.subscribe((data)=>{
+      var con = data;
+      console.log("popup",con);
+    });
     let driverdata = {
       DriverName :this.driverform.DriverName.value,
       LicenseIssueDate :this.datePipe.transform(this.driverform.LicenseIssueDate.value,  'yyyy-MM-dd'),
